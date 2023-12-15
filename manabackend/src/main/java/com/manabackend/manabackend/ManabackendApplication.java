65f571320d76d5db6eedfb24fe28c5416d3a455c
sitesparkco <sitesparkco@gmail.com>
2023-12-15 16:52:39 +0530

    setting project env

---
+++ manabackend/src/main/java/com/manabackend/manabackend/ManabackendApplication.java
@@ -0,0 +1,13 @@
+package com.manabackend.manabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManabackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManabackendApplication.class, args);
+	}
+
+}
---
+++ manabackend/src/test/java/com/manabackend/manabackend/ManabackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manabackend.manabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManabackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
